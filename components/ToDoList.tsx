0575502a4e67ec41f90c084b9d5232249685da36
Jonathan Cannizzaro <jojo.can.94@gmail.com>
2024-11-29 01:17:33 +0100

    Feat(ToDoList, ToDoItem, Page): Added remove task functionality, restyling of task item

--- components/ToDoList.tsx
+++ components/ToDoList.tsx
@@ -1,19 +1,32 @@
 "use client";
 import { useEffect, useState } from "react";
 import ToDoItem from "./ToDoItem";
+import { v4 as uuidv4 } from "uuid";
+export interface ToDo {
+    id: string;
+    text: string;
+  }
 
 export default function ToDoList(props: {todoInput: string}) {
-    const [toDoItems, setToDoItems] = useState<string[]>([])
+    const [toDoItems, setToDoItems] = useState<ToDo[]>([])
+
+    const onRemove = (idItem: string) => {
+        setToDoItems((prevItems) => prevItems.filter((item) => item.id !== idItem));
+    }
 
     useEffect(() => {
         if(props.todoInput) {
-            setToDoItems((prevItems) => [...prevItems, props.todoInput]);
+            const newToDo = {
+                id: uuidv4(),
+                text: props.todoInput,
+            };
+            setToDoItems((prevItems) => [...prevItems, newToDo]);
         }
     }, [props.todoInput]);
     return (
         <div>
-            {toDoItems.map((item, index) => (
-                <ToDoItem key={index} todoInput={item}/>
+            {toDoItems.map((item) => (
+                <ToDoItem key={item.id} todoInput={item} onRemove={onRemove}/>
             ))}           
         </div>
     )
